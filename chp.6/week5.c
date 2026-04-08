355c5dabaa3c78330b84a9ceeef0e95ce878cd4a
lomio6098-l2zm <lomio6098@gmail.com>
2026-04-08 14:57:49 +0900

    sorry

---
+++ chp.6/week5.c
@@ -0,0 +1,40 @@
+﻿#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<string> hcat(const vector<string>& left, const vector<string>& right)
+{
+    vector<string> ret;
+
+    // 왼쪽 문자열 최대 길이 + 공백 1칸
+    string::size_type width1 = 0;
+    for (size_t i = 0; i < left.size(); i++)
+    {
+        if (left[i].size() > width1)
+            width1 = left[i].size();
+    }
+    width1 += 1;
+
+    vector<string>::size_type i = 0, j = 0;
+
+    while (i != left.size() || j != right.size())
+    {
+        string s;
+
+        // 왼쪽 문자열 추가
+        if (i != left.size())
+            s = left[i++];
+
+        // 공백 채우기
+        s += string(width1 - s.size(), ' ');
+
+        // 오른쪽 문자열 추가
+        if (j != right.size())
+            s += right[j++];
+
+        ret.push_back(s);
+    }
+
+    return 0;
+}
---
+++ chp.6/week6.c
@@ -0,0 +1,43 @@
+﻿#include <vector>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+// 공백이면 true
+bool space(char c)
+{
+    return isspace(c);
+}
+
+// 공백이 아니면 true
+bool not_space(char c)
+{
+    return !isspace(c);
+}
+
+vector<string> split(const string& str)
+{
+    typedef string::const_iterator iter;
+    vector<string> ret;
+
+    iter i = str.begin();
+
+    while (i != str.end())
+    {
+        // 앞쪽 공백 제거
+        i = find_if(i, str.end(), not_space);
+
+        // 단어 끝 찾기
+        iter j = find_if(i, str.end(), space);
+
+        // 범위 내 문자열 저장
+        if (i != str.end())
+            ret.push_back(string(i, j));
+
+        i = j;
+    }
+
+    return 0;
+}
